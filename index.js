4709c87a5e4d4f04a7817087cf8d60a716e361b2
Taylor Williams <taylor@thewilliams.net>
2019-02-05 02:38:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, match){
-  const newDrivers
+  const newDrivers = []
   for(const driver of drivers){
     if (driver.toUpperCase() === match.toUpperCase()) {
       newDrivers.push(driver)
